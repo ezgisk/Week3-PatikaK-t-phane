b950b60db6487fa4d56261cef43d14ae3d8e6fee
Gorkem <Gorkem@DESKTOP-2DV200H>
2024-10-14 22:43:43 -0700

    Add project files.

---
+++ Week3-PatikaKütüphane/Books.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_PatikaKütüphane
+{
+    public class Books
+    {
+        //Properties
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorSurname { get; set; }
+        public int PageNumber{ get; set; }
+        public string PublishingHouse { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        //Defoult constructor
+        public Books()
+        {
+            this.RegistrationDate = DateTime.Now;
+         
+        }
+        //Paremeter constructor
+        public Books(string bookName, string authorName,string authorSurname, int pageNumber,string publishingHouse)
+        {
+            BookName = bookName;   
+            AuthorName = authorName;
+            AuthorSurname = authorSurname;
+            PageNumber = pageNumber;
+            PublishingHouse = publishingHouse;
+            RegistrationDate = DateTime.Now;
+            
+
+        }
+        //Class: Benzer özelliklere ve davranışlara sahip nesneleri tanımlayan bir şablondur.
+
+        //Property: Sınıf içinde tanımlanan, nesne verilerine erişim sağlayan değişkenlerdir.
+
+        //New: Yeni bir nesne oluşturmak için kullanılan anahtar kelimedir.
+
+        //Constructor: Bir sınıfın nesnesi oluşturulduğunda otomatik olarak çağrılan ve başlangıç değerlerini atayan özel bir metottur.
+    }
+}
---
+++ Week3-PatikaKütüphane/Program.cs
@@ -0,0 +1,11 @@
+﻿using Week3_PatikaKütüphane;
+
+Books book1 = new Books("Aylin", "Ayşe","Kulin", 398, "Remzi Kitabevi");
+
+Console.WriteLine($"Kitap Adı: {book1.BookName}, Yazar: {book1.AuthorName} {book1.AuthorSurname}, Sayfa Sayısı: {book1.PageNumber}, Yayınevi: {book1.PublishingHouse}, Kayıt Tarihi: {book1.RegistrationDate}");
+
+Books book2 = new Books("Ben yazdim", "Ezgi","Gecimli", 158, "Gecimli Kitabevi");
+
+Console.WriteLine($"Kitap Adı: {book2.BookName}, Yazar: {book2.AuthorName} {book2.AuthorSurname}, Sayfa Sayısı: {book2.PageNumber}, Yayınevi: {book2.PublishingHouse}, Kayıt Tarihi: {book2.RegistrationDate}");
+
+Console.ReadKey();
